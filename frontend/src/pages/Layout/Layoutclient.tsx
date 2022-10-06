ec11a977cca7cb8e0f77809033c874f984b7d402
TuanTuNguyen <tuntph14942@fpt.edu.vn>
2022-10-07 00:51:07 +0700

    feat : update frontend

---
+++ frontend/src/pages/Layout/Layoutclient.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+
+
+
+const Layoutclient = () => {
+  return (
+    <div>
+       <header>đây là header</header>
+        <main>
+            <Outlet />
+        </main>
+        <footer> đây là footer</footer>
+    </div>
+  )
+}
+
+export default Layoutclient
