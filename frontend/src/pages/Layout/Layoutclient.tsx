5c02d5bc7d23f41a83ad88d73a57ad003f43b3b6
Duc Mai Thanh <ducmtph14090@fpt.edu.vn>
2022-10-07 14:33:28 +0700

    ductesst

--- frontend/src/pages/Layout/Layoutclient.tsx
+++ frontend/src/pages/Layout/Layoutclient.tsx
@@ -7,10 +7,12 @@ import { Outlet } from 'react-router-dom'
 const Layoutclient = () => {
   return (
     <div>
+      <h1>Đức Mt</h1>
        <header>đây là header</header>
         <main>
             <Outlet />
         </main>
+
         <footer> đây là footer</footer>
     </div>
   )
