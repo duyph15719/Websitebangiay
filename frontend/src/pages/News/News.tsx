b747997800de91d4a272d1ad6e57ab2ac5269ff5
TuanTuNguyen <tuntph14942@fpt.edu.vn>
2022-10-14 19:40:24 +0700

    feat : create pagenews and newsdetail

---
+++ frontend/src/pages/News/News.tsx
@@ -0,0 +1,112 @@
+import React from 'react'
+import NavBarNews from '../../components/navbarnews/navbarnews'
+
+import './News.css'
+
+const NewsPage = () => {
+    return (
+    
+        <div className='news__page'>
+            < NavBarNews/>
+            <div className="main__news">
+                <h1 className="title__main__news">
+                    TIN TỨC
+                </h1>
+                <div className="list__main__news">
+                    <div className="item__list__main__news">
+                        <div className="img__main__news">
+                            <a href="">
+                                <img src="https://drake.vn/image/catalog/H%C3%ACnh%20content/Converse-Platform/converse-platform-01.jpg" alt="" />
+                            </a>
+                        </div>
+                        <div className="desc__item__main">
+                            <h3 className="title__desc__item">
+                                Converse sẽ mang Golf le Fleur* Chuck 70 về Việt Nam?
+                            </h3>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut in exercitationem maxime ullam molestiae sed quaerat eaque rerum ipsum dolorem! Earum ipsam voluptatem quo veniam praesentium, obcaecati sed facere recusandae.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="item__list__main__news">
+                        <div className="img__main__news">
+                            <a href="">
+                                <img src="https://drake.vn/image/catalog/H%C3%ACnh%20content/Converse-Platform/converse-platform-01.jpg" alt="" />
+                            </a>
+                        </div>
+                        <div className="desc__item__main">
+                            <h3 className="title__desc__item">
+                                Converse sẽ mang Golf le Fleur* Chuck 70 về Việt Nam?
+                            </h3>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut in exercitationem maxime ullam molestiae sed quaerat eaque rerum ipsum dolorem! Earum ipsam voluptatem quo veniam praesentium, obcaecati sed facere recusandae.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="item__list__main__news">
+                        <div className="img__main__news">
+                            <a href="">
+                                <img src="https://drake.vn/image/catalog/H%C3%ACnh%20content/Converse-Platform/converse-platform-01.jpg" alt="" />
+                            </a>
+                        </div>
+                        <div className="desc__item__main">
+                            <h3 className="title__desc__item">
+                                Converse sẽ mang Golf le Fleur* Chuck 70 về Việt Nam?
+                            </h3>
+<p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut in exercitationem maxime ullam molestiae sed quaerat eaque rerum ipsum dolorem! Earum ipsam voluptatem quo veniam praesentium, obcaecati sed facere recusandae.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="item__list__main__news2">
+                        <div className="img__main__news">
+                            <a href="">
+                                <img src="https://drake.vn/image/catalog/H%C3%ACnh%20content/Converse-Platform/converse-platform-01.jpg" alt="" />
+                            </a>
+                        </div>
+                        <div className="desc__item__main">
+                            <h3 className="title__desc__item">
+                                Converse sẽ mang Golf le Fleur* Chuck 70 về Việt Nam?
+                            </h3>
+<p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut in exercitationem maxime ullam molestiae sed quaerat eaque rerum ipsum dolorem! Earum ipsam voluptatem quo veniam praesentium, obcaecati sed facere recusandae.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="item__list__main__news2">
+                        <div className="img__main__news">
+                            <a href="">
+                                <img src="https://drake.vn/image/catalog/H%C3%ACnh%20content/Converse-Platform/converse-platform-01.jpg" alt="" />
+                            </a>
+                        </div>
+                        <div className="desc__item__main">
+                            <h3 className="title__desc__item">
+                                Converse sẽ mang Golf le Fleur* Chuck 70 về Việt Nam?
+                            </h3>
+<p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut in exercitationem maxime ullam molestiae sed quaerat eaque rerum ipsum dolorem! Earum ipsam voluptatem quo veniam praesentium, obcaecati sed facere recusandae.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="item__list__main__news2">
+                        <div className="img__main__news">
+                            <a href="">
+                                <img src="https://drake.vn/image/catalog/H%C3%ACnh%20content/Converse-Platform/converse-platform-01.jpg" alt="" />
+                            </a>
+                        </div>
+                        <div className="desc__item__main">
+                            <h3 className="title__desc__item">
+                                Converse sẽ mang Golf le Fleur* Chuck 70 về Việt Nam?
+                            </h3>
+<p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut in exercitationem maxime ullam molestiae sed quaerat eaque rerum ipsum dolorem! Earum ipsam voluptatem quo veniam praesentium, obcaecati sed facere recusandae.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NewsPage
